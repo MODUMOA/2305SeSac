6ec2d014a871baa0aa527e2ae3aa3bc8e924f91a
JM <k3371548@gmail.com>
2023-06-01 21:21:31 +0900

    JM commit

--- src/components/notice/list/List.vue
+++ src/components/notice/list/List.vue
@@ -4,13 +4,35 @@
       <img :src="this.$resource.commonRes.img('15x14_notice_icon.png')" alt="공지사항 아이콘" style="width: 15px;"/>
         <span>전체 공지사항</span>
     </div>
-    <ul class="col-12 notice_list_con">
+    <ul class="col-12 mb50 notice_list_con">
       <notice-list-item
               v-for="list in lists"
               :key="list.no"
               :list="list"
               v-bind="list"
             ></notice-list-item>
+    </ul>
+    <ul class="col-12 pagination_style_0">
+      <li class="paging_btn_single_page to_first"></li>
+      <li class="paging_btn_single_page to_prev"></li>
+
+        <li class="paging_btn_page selected">
+          <a href="javascript:void(0)">1</a>
+        </li>
+        <li class="paging_btn_page">
+          <a href="javascript:void(0)">2</a>
+        </li>
+        <li class="paging_btn_page">
+          <a href="javascript:void(0)">3</a>
+        </li>
+        <li class="paging_btn_page">
+          <a href="javascript:void(0)">4</a>
+        </li>
+        <li class="paging_btn_page">
+          <a href="javascript:void(0)">5</a>
+        </li>
+        <li class="paging_btn_single_page to_next"></li>
+        <li class="paging_btn_single_page to_last"></li>
     </ul>
   </div>
 </template>
