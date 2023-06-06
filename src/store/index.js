d40806f82334b598742ec87953175a12461d484a
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-06-06 21:45:00 +0900

    fix.vuex 라이브러리 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,24 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
-import userStore from '@/store/modules/userStore';
+import createPersistedState from "vuex-persistedstate";
+import userStore from "@/store/modules/userStore";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     userStore,
   },
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  
-})
+  plugins: [
+    createPersistedState({
+      // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
+      storage: sessionStorage,
+    }),
+  ],
+
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+});
