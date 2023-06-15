b4ec55e097c45f842a9536d775b3cb9cdaa6a047
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-06-15 23:42:26 +0900

    fix. 최종점검

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: "http://43.202.9.237:8080/",
+//   timeout: 100000,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
 export default axios.create({
-  baseURL: "http://43.202.9.237:8080/",
+  baseURL: "http://localhost:8080/",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
